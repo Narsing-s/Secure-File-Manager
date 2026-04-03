bd74e237f6df449a0ce8cf198032cff40a114a48
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-04-03 12:46:32 +0530

    Delete MainActivity.kt

--- MainActivity.kt
+++
@@ -1,8 +0,0 @@
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            VaultFilesApp()
-        }
-    }
-}
